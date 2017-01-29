880805d3eecc7af2a91f78eea6d4358f809588e1
Dan Gorman <danielhgorman@gmail.com>
2017-01-29 14:30:32 -0500

    updated cli and food class

--- lib/resturant/food.rb
+++ lib/resturant/food.rb
@@ -1,20 +1,22 @@
 class Resturant::Food
-attr_accessor :name, :map, :img, :description
+  attr_accessor :name, :map, :img, :description
+  FOOD = []
 
-def self.available_resturant
-self.scraper_resturant_site
-end
+  def self.available_resturant
+    self.scraper_resturant_site
+  end
 
-def self.scraper_resturant_site
-Food = []
-doc = Nokogiri::HTML(open('https://www.tripadvisor.co.uk/Restaurants-g659505-Erbil_Erbil_Province.html'))
-self.scraper_resturant_site do |site|
-resturant = Resturant.new
-resturant.name = site.css("a.property_title").text
-resturant.map = site.css("a#restmap").text
-resturant.description = site.css("a.cuisine").text
-end
-end
+  def self.scraper_resturant_site
+    binding.pry
+    doc = Nokogiri::HTML(open('https://www.tripadvisor.co.uk/Restaurants-g659505-Erbil_Erbil_Province.html'))
+    # This is an infinite loop!
+    self.scraper_resturant_site do |site|
+      resturant = Resturant.new
+      resturant.name = site.css("a.property_title").text
+      resturant.map = site.css("a#restmap").text
+      resturant.description = site.css("a.cuisine").text
+    end
+  end
 
 
   def print_resturant
@@ -27,12 +29,9 @@ end
       end
     end
   end
-
-
-
-food << resturant
-end
-food
-end
+  #food << resturant
+  #end
+  #food
+  #end
 
 end
